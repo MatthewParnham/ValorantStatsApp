a7bfee507876272e3fab75d168624bb34cd37acb
matthewparnham <parnham@chapman.edu>
2020-05-07 17:32:43 -0700

    working on creating primary form.

--- ValorantStatsApp/Form1.Designer.cs
+++ ValorantStatsApp/Form1.Designer.cs
@@ -28,13 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.MapComboBox = new System.Windows.Forms.ComboBox();
+            this.HeroComboBox = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(65, 100);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(529, 378);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 455);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "MatchDetails",
+            "Scoreboard",
+            "Performance",
+            "Timeline"});
+            this.comboBox1.Location = new System.Drawing.Point(409, 50);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // MapComboBox
+            // 
+            this.MapComboBox.FormattingEnabled = true;
+            this.MapComboBox.Location = new System.Drawing.Point(225, 50);
+            this.MapComboBox.Name = "MapComboBox";
+            this.MapComboBox.Size = new System.Drawing.Size(121, 21);
+            this.MapComboBox.TabIndex = 3;
+            this.MapComboBox.SelectedIndexChanged += new System.EventHandler(this.MapComboBox_SelectedIndexChanged);
+            // 
+            // HeroComboBox
+            // 
+            this.HeroComboBox.FormattingEnabled = true;
+            this.HeroComboBox.Location = new System.Drawing.Point(91, 49);
+            this.HeroComboBox.Name = "HeroComboBox";
+            this.HeroComboBox.Size = new System.Drawing.Size(121, 21);
+            this.HeroComboBox.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1584, 861);
+            this.Controls.Add(this.HeroComboBox);
+            this.Controls.Add(this.MapComboBox);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox MapComboBox;
+        private System.Windows.Forms.ComboBox HeroComboBox;
     }
 }
 
--- ValorantStatsApp/Form1.cs
+++ ValorantStatsApp/Form1.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using System.Diagnostics;
 
 namespace ValorantStatsApp
 {
     public partial class Form1 : Form
     {
+
+        private string ConString = ConfigurationManager.ConnectionStrings["ConString"].ConnectionString;
+        private MySqlConnection con;
+
         public Form1()
         {
             InitializeComponent();
+            con = new MySqlConnection(ConString);
+            loadComboBoxes();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void loadComboBoxes()
+        {
+            string query = "SELECT Name FROM Maps";
+            DataTable output = CreateQuery(query);
+            foreach(DataRow row in output.Rows)
+            {
+                MapComboBox.Items.Add(row[0]);
+            }
+            query = "SELECT Name FROM Heroes";
+            output = CreateQuery(query);
+            foreach (DataRow row in output.Rows)
+            {
+                HeroComboBox.Items.Add(row[0]);
+            }
+
+            MapComboBox.Items.Insert(0,"Any");
+            HeroComboBox.Items.Insert(0, "Any");
+            MapComboBox.SelectedIndex = 0;
+            HeroComboBox.SelectedIndex = 0;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string CmdString = "SELECT Heroes.Name, COUNT(Hero) FROM Scoreboard JOIN Heroes on Hero = HeroID GROUP BY Heroes.Name ORDER BY COUNT(Hero)";
+            DataTable output = CreateQuery(CmdString);
+            dataGridView1.DataSource = output.DefaultView;
+            
+        }
+
+        private DataTable CreateQuery(string CmdString)
+        {
+            MySqlDataAdapter sda = new MySqlDataAdapter(CmdString, con);
+            DataSet ds = new DataSet();
+            sda.Fill(ds);
+            return ds.Tables[0];
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MapComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
